86a897cf9627e63003438deb1242b54e4516e50d
kulsoom <adnanirfan282@gmail.com>
2025-12-02 22:32:10 +0500

    chagne the baseURl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://kulsoomadnan.github.io/',
+  url: 'https://kulsoomadnan.github.io/docusaurus-demo',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-demo',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
