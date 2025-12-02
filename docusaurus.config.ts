70c2955ad756032772fdc0bfb169348958883be9
kulsoom <adnanirfan282@gmail.com>
2025-12-02 19:05:07 +0500

    Add GitHub Actions deploy workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://kulsoomadnan.github.io/docusaurus-demo/',
+  url: 'https://kulsoomadnan.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-demo/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
