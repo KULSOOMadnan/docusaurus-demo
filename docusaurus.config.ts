eb9027ca28c10c062765d0dc77e6404a46d85b8a
kulsoom <adnanirfan282@gmail.com>
2025-12-02 22:34:55 +0500

    fixing base url error

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://kulsoomadnan.github.io/docusaurus-demo',
+  url: 'https://kulsoomadnan.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
